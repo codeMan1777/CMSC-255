151bd2388c892d922edc5cc504af9f702a2f6a0c
codeMan1777 <112028483+codeMan1777@users.noreply.github.com>
2022-08-24 17:41:08 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*********************************************************
+* Lab 01
+********************************************************** 
+* Robert Bray
+* 08-24-2022
+* 255 001
+*********************************************************/
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("<Robert Bray>");
+		System.out.println("<Lab 01>");
+		System.out.println("<The goal of the lab is to run and install java.>");
+		System.out.println("<08-24-2022>");
+		System.out.println("<255 001>"); 
+		System.out.println();
+	
+	}
+}
